24bb4f6dfbca76104ace22e509532b260441cc93
Kevin Osei <knokwaku@gmail.com>
2023-09-17 22:13:22 +0000

    added navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import styles from './style';
 import { Navbar, Hero, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials } from './components';
 
@@ -6,7 +6,7 @@ const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        {/* <Navbar />  */}
+        <Navbar />
       </div>
     </div>
 
