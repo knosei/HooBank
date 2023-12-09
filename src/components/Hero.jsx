75f72e93400e626d281ea74043eccfa4e31e6fbe
Kevin Osei <knokwaku@gmail.com>
2023-12-09 21:11:12 +0000

    added statistics below hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,8 +25,8 @@ const Hero = () => (
 
       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] 
       leading-[75px] w-full">Payment Method.</h1>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most 
-      likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual fees.</p>
     </div>
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
