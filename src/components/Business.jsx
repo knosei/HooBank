21ca8d72d9cd513ef3dc58c0ce5e9a1f2985a41a
Kevin Osei <knokwaku@gmail.com>
2023-12-21 21:51:59 +0000

    feature cards for business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,14 @@ import { features } from "../constants";
 import styles, { layout } from '../style';
 import Button from './Button';
 
+const FeatureCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"}`}>
+    <div>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -15,6 +23,12 @@ const Business = () => {
         <Button styles="mt-10"/>
       </div>
 
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+
+      </div>
     </section>
   )
 }
