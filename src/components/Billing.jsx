5aa952f6c3c4ea1e8e169f2c5a8ab01df1aaf70f
Kevin Osei <knokwaku@gmail.com>
2024-02-05 22:03:55 +0000

    text for billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,6 +9,12 @@ const Billing = () => (
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
     </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your 
+        <br className='sm:block hidden'/> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Nullam porta hendrerit quam, cursus rhoncus nisi rutrum eget. Nullam id pharetra libero. Aliquam interdum, felis quis tempus luctus.</p>
+    </div>
   </section>
 )
 
