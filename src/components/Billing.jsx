9835f237dffc5c0e58bc4dcb621959bc525ee783
Kevin Osei <knokwaku@gmail.com>
2024-02-12 22:35:41 +0000

    app store image

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,14 +6,22 @@ const Billing = () => (
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
-      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
     </div>
 
     <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>Easily control your 
-        <br className='sm:block hidden'/> billing & invoicing.</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Nullam porta hendrerit quam, cursus rhoncus nisi rutrum eget. Nullam id pharetra libero. Aliquam interdum, felis quis tempus luctus.</p>
+      <h2 className={styles.heading2}>Easily control your
+        <br className='sm:block hidden' /> billing & invoicing.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Nullam porta hendrerit quam, cursus rhoncus nisi rutrum eget.
+        Nullam id pharetra libero. Aliquam interdum, felis quis tempus
+        luctus.
+      </p>
+      
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="google_play" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+      </div>
     </div>
   </section>
 )
