3830266c22c956536c948183c77fc7c4052eb9b2
Kevin Osei <knokwaku@gmail.com>
2024-02-22 21:40:41 +0000

    testimonial cards

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import { feedback } from "../constants";
 import styles from "../style";
-// import FeedbackCard from './FeedbackCard';
+import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
@@ -12,9 +12,16 @@ const Testimonials = () => (
         <h1 className={styles.heading2}>What people are <br className="sm:block hidden"/> 
         saying about us</h1>
         <div className="w-full md:mt-0 mt-6">
-          <p className={`${styles.paragraph} text-left max-w-[45]`}>Everything you need to accept 
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept 
           card payments and grow your business anywhere on the planet.</p>
         </div>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container 
+      relative z-[1]">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+
       </div>  
     </section>
 )
