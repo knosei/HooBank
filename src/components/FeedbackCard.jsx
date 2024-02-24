5d1620bd5fe243d2f43a38d3a628e530a548880e
Kevin Osei <knokwaku@gmail.com>
2024-02-24 21:40:27 +0000

    testimonial images and layout

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { quotes } from '../assets';
 
 const FeedbackCard = ({ content, name, title, img}) => (
     <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]
     max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="double_quotes" className="w-[42px] h-[27px] object-contain"/>
+      <p className="font-poppins font-normal text-[18px] leading-[32px] text-white">{content}</p>
 
+      <div className="flex flex-row">
+        <img src={img} alt={name} className='w-[48px] h-[48px] rounded-full' />
+      </div>
     </div>
 )
 
