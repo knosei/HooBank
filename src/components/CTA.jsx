379dbcf88410c237f2c8393e493cd6abedcc2022
Kevin Osei <knokwaku@gmail.com>
2024-03-06 21:42:18 +0000

    footer section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,10 +6,11 @@ const CTA = () => (
     sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className="flex-1 flex flex-col">
         <h2 className={styles.heading2}>Let's try our service now!</h2>
-        <p className={`${styles.paragraph} max-w-[470PX] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        <p className={`${styles.paragraph} max-w-[470PX] mt-5`}>
+        Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
 
-      <div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Button />
       </div>
     </section>
