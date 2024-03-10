280a99f1e46dfe669d1be22819779bca822dbad7
Kevin Osei <knokwaku@gmail.com>
2024-03-10 21:00:44 +0000

    social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,10 +15,32 @@ const Footer = () => (
       flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
           <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-            <h4>
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerLink.title}
             </h4>
+            <ul className="list-none mt-4">
+              {footerLink.links.map((Link, index) => (
+                <li key={Link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px]
+                   text-dimWhite hover:text-secondary cursor-pointer 
+                   ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
+                  {Link.name}
+                </li>
+              ))}
+            </ul>
           </div>
+        ))}
+      </div>
+    </div>
+
+    <div>
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        2024 HooBank. All Rights Reserved
+      </p>
+
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img key={social.id} src={social.icon} alt={social.id}/>
         ))}
       </div>
     </div>
